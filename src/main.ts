27c53ea85b2ccfa79c3ea9481f82fdc4a8c66121
chengyun <1058148367@qq.com>
2020-12-25 14:49:26 +0800

    修改element中文

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import router from "./router"
 import store from "./store"
 import ElementPlus from "element-plus"
 import "element-plus/lib/theme-chalk/index.css"
+import 'dayjs/locale/zh-cn'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(ElementPlus)
+  .use(ElementPlus,{ locale })
   .mount("#app")
